7b023e6b993d8df42071f55577887167c549b131
Christian Holm <cho@cubitech.dk>
2022-12-04 16:14:53 +0700

    Day 1 part 2

--- src/2022/1/index.ts
+++ src/2022/1/index.ts
@@ -1,24 +1,31 @@
 import fs from "fs/promises";
-import { max, reverse, sortBy, sum } from "lodash";
+import { identity, last, sortBy, sum } from "lodash";
 import { join } from "path";
 
-async function loadInput(): Promise<string> {
-  return await fs.readFile(join(__dirname, "input.txt"), {
+async function loadInput(): Promise<number[][]> {
+  const data = await fs.readFile(join(__dirname, "..", "1", "input.txt"), {
     encoding: "utf-8",
   });
+
+  const elfs = data.split("\n\n");
+  const elfWithCalories = elfs.map((calories) =>
+    calories.split("\n").filter(identity)
+  );
+
+  return elfWithCalories.map((calories) =>
+    calories.map((calory) => parseInt(calory))
+  );
 }
 
 async function main() {
-  const data = await loadInput();
+  const elfs = await loadInput();
 
-  const elfs = data.split("\n\n");
-  const elfCalories = elfs
-    .map((calories) => calories.split("\n").map((calory) => parseInt(calory)))
-    .map((calories) => sum(calories));
+  const elfTotalCalories = elfs.map((calories) => sum(calories));
 
-  const mostCalories = max(elfCalories);
+  const sortedByCalories = sortBy(elfTotalCalories, identity);
 
-  console.log(mostCalories);
+  console.log(last(sortedByCalories));
+  console.log(sum(sortedByCalories.slice(-3)));
 }
 
 main();
