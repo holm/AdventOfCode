878edca24d06941b58bf6afe8fb2ebf467028f17
Christian Holm <cho@cubitech.dk>
2024-02-19 10:07:16 +0100

    Day 15 part 2

--- src/2023/15/index.ts
+++ src/2023/15/index.ts
@@ -1,9 +1,10 @@
+import assert from "assert";
 import fs from "fs/promises";
-import { sumBy } from "lodash";
+import { sum, sumBy, times } from "lodash";
 import { join } from "path";
 
 async function loadInput(): Promise<string[]> {
-  const data = await fs.readFile(join(__dirname, "example.txt"), {
+  const data = await fs.readFile(join(__dirname, "input.txt"), {
     encoding: "utf-8",
   });
 
@@ -32,15 +33,59 @@ async function part1() {
 }
 
 async function part2() {
-  const grid = await loadInput();
+  const steps = await loadInput();
 
-  const result = "";
+  const map = times(256).map(() => [] as [string, number][]);
+
+  for (const step of steps) {
+    let label: string;
+    let operation: "=" | "-";
+    let value: number | null;
+
+    if (step.includes("=")) {
+      operation = "=";
+      const parts = step.split("=");
+      label = parts[0];
+      value = parseInt(parts[1]);
+    } else {
+      operation = "-";
+      label = step.substring(0, step.length - 1);
+      value = null;
+    }
+
+    const hashedLabel = computeHash(label);
+    const box = map[hashedLabel];
+
+    const existingIdx = box.findIndex(([l]) => l === label);
+
+    if (operation === "=") {
+      assert(value !== null);
+      if (existingIdx !== -1) {
+        box[existingIdx][1] = value;
+      } else {
+        box.push([label, value]);
+      }
+    } else if (operation === "-") {
+      if (existingIdx !== -1) {
+        box.splice(existingIdx, 1);
+      }
+    }
+  }
+
+  const scores = map.map((box, boxIdx) => {
+    return (
+      (boxIdx + 1) *
+      sum(box.map((entry, entryIdx) => (entryIdx + 1) * entry[1]))
+    );
+  });
+
+  const result = sum(scores);
   console.log("part2", result);
 }
 
 async function main() {
   await part1();
-  // await part2();
+  await part2();
 }
 
 main();
