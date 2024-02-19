a7c39a17d769183adbd502efaa7ffd58f595114b
Christian Holm <cho@cubitech.dk>
2024-02-19 08:38:34 +0100

    Day 13 part 2

--- src/2023/13/index.ts
+++ src/2023/13/index.ts
@@ -14,20 +14,53 @@ async function loadInput(): Promise<Pattern[]> {
   });
 }
 
-function horizontalReflection(pattern: Pattern): number | undefined {
-  outer: for (let r = 0; r < pattern.length - 1; r++) {
-    const toCheck = Math.min(r + 1, pattern.length - r - 1);
-    for (let m = 0; m < toCheck; m++) {
-      if (pattern[r - m] !== pattern[r + m + 1]) {
-        continue outer;
+function isHorizontalReflection(
+  pattern: Pattern,
+  smudged: boolean,
+  r: number
+): boolean {
+  let smudgeUsed = !smudged;
+
+  const mMax = Math.min(r + 1, pattern.length - r - 1);
+  for (let m = 0; m < mMax; m++) {
+    const above = pattern[r - m];
+    const below = pattern[r + m + 1];
+
+    if (smudgeUsed) {
+      if (above !== below) {
+        return false;
+      }
+    } else {
+      for (let c = 0; c < above.length; c++) {
+        if (above.charAt(c) !== below.charAt(c)) {
+          if (smudgeUsed) {
+            return false;
+          } else {
+            smudgeUsed = true;
+          }
+        }
       }
     }
+  }
 
-    return r + 1;
+  return smudgeUsed;
+}
+
+function horizontalReflection(
+  pattern: Pattern,
+  smudged: boolean
+): number | undefined {
+  for (let r = 0; r < pattern.length - 1; r++) {
+    if (isHorizontalReflection(pattern, smudged, r)) {
+      return r + 1;
+    }
   }
 }
 
-function verticalReflection(pattern: Pattern): number | undefined {
+function verticalReflection(
+  pattern: Pattern,
+  smudged: boolean
+): number | undefined {
   const rotated: Pattern = [];
 
   const reversedPattern = [...pattern].reverse();
@@ -36,33 +69,42 @@ function verticalReflection(pattern: Pattern): number | undefined {
     rotated.push(reversedPattern.map((line) => line.charAt(c)).join(""));
   }
 
-  return horizontalReflection(rotated);
+  return horizontalReflection(rotated, smudged);
+}
+
+function computeScore(patterns: Pattern[], smudged: boolean): number {
+  return sumBy(patterns, (pattern) => {
+    const h = horizontalReflection(pattern, smudged);
+    if (h !== undefined) {
+      return 100 * h;
+    }
+
+    const v = verticalReflection(pattern, smudged);
+    if (v !== undefined) {
+      return v;
+    }
+
+    throw new Error("No reflection");
+  });
 }
 
 async function part1() {
   const patterns = await loadInput();
 
-  const result = sumBy(patterns, (pattern) => {
-    const h = horizontalReflection(pattern);
-    if (h !== undefined) {
-      return 100 * h;
-    }
-
-    return verticalReflection(pattern) || 0;
-  });
+  const result = computeScore(patterns, false);
   console.log("part1", result);
 }
 
 async function part2() {
-  const grid = await loadInput();
+  const patterns = await loadInput();
 
-  const result = grid.toString();
+  const result = computeScore(patterns, true);
   console.log("part2", result);
 }
 
 async function main() {
   await part1();
-  // await part2();
+  await part2();
 }
 
 main();
