da3c8cd3f19f6d5e2bd958fc3e6d6e04916ab9ef
Christian Holm <cho@cubitech.dk>
2024-12-08 10:43:34 +0100

    2024 Day 7 Part 2
    Signed-off-by: Christian Holm <cho@cubitech.dk>

--- src/2024/7/index.ts
+++ src/2024/7/index.ts
@@ -6,6 +6,7 @@ type Equation = {
   result: number;
   values: number[];
 };
+type Operator = "+" | "*" | "||";
 
 async function loadInput(): Promise<Equation[]> {
   const data = await fs.readFile(join(__dirname, "input.txt"), {
@@ -25,49 +26,72 @@ async function loadInput(): Promise<Equation[]> {
     });
 }
 
-function isValidEquation(equation: Equation): boolean {
+const operatorFunctions: Record<
+  Operator,
+  (lValue: number, rValue: number) => number
+> = {
+  "+": (lValue: number, rValue: number) => {
+    return lValue + rValue;
+  },
+  "*": (lValue: number, rValue: number) => {
+    return lValue * rValue;
+  },
+  "||": (lValue: number, rValue: number) => {
+    return parseInt(`${lValue}${rValue}`);
+  },
+};
+
+function isValidEquation(equation: Equation, operators: Operator[]): boolean {
   const { result, values } = equation;
 
   if (values.length === 1) {
     return values[0] === result;
   }
 
-  const addEquation = {
-    result,
-    values: [values[0] + values[1], ...values.slice(2)],
-  };
-  if (isValidEquation(addEquation)) {
-    return true;
-  }
+  for (const operator of operators) {
+    const [lValue, rValue, ...restValues] = values;
 
-  const mulEquation = {
-    result,
-    values: [values[0] * values[1], ...values.slice(2)],
-  };
-  if (isValidEquation(mulEquation)) {
-    return true;
+    const newValue = operatorFunctions[operator](lValue, rValue);
+
+    const newEquation = {
+      result,
+      values: [newValue, ...restValues],
+    };
+    if (isValidEquation(newEquation, operators)) {
+      return true;
+    }
   }
 
   return false;
 }
 
+function getScore(equations: Equation[]): number {
+  return sum(equations.map((equation) => equation.result));
+}
+
 async function part1(): Promise<number> {
-  const input = await loadInput();
+  const equations = await loadInput();
 
-  const validEquations = input.filter((equation) => isValidEquation(equation));
+  const validEquations = equations.filter((equation) =>
+    isValidEquation(equation, ["+", "*"])
+  );
 
-  return sum(validEquations.map((equation) => equation.result));
+  return getScore(validEquations);
 }
 
 async function part2(): Promise<number> {
-  const input = await loadInput();
+  const equations = await loadInput();
 
-  return 0;
+  const validEquations = equations.filter((equation) =>
+    isValidEquation(equation, ["+", "*", "||"])
+  );
+
+  return getScore(validEquations);
 }
 
 async function main() {
   console.log(await part1());
-  // console.log(await part2());
+  console.log(await part2());
 }
 
 main();
